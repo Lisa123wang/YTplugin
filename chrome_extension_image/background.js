bc74a893df994fc68b30630423d830947b5169ba
Lisa <wengling375@gmail.com>
2024-03-26 16:55:17 +0800

    name

--- chrome_extension_image/background.js
+++ chrome_extension_image/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.contentScriptQuery === "sendImageUrl") {
-      const url = 'https://4c35-140-136-63-20.ngrok-free.app/325/imageReceiver.php'; // Replace with your PHP server URL
+      const url = 'https://4c35-140-136-63-20.ngrok-free.app/YTplugin/chrome_extension_image/imageReceiver.php'; // Replace with your PHP server URL
       fetch(url, {
         method: 'POST',
         body: JSON.stringify({imageUrl: request.imageUrl}),
